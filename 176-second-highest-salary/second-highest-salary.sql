caa637acebceac4bb94eaad2426e849f87a2cd22
Hari Chandana Kotnani <122574307+Harichandana0301@users.noreply.github.com>
2025-01-13 14:01:05 -0700

    Time: 604 ms (52.11%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,3 @@
 /* Write your T-SQL query statement below */
-select max(salary) as SecondHighestSalary from Employee
+select isnull(max(salary),null) as SecondHighestSalary from Employee
 where salary < (select max(salary) as FirstHighestSalary from Employee)
