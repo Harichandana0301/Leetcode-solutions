bc766e378e9f62379656c194ff8b682f046bcef8
Hari Chandana Kotnani <122574307+Harichandana0301@users.noreply.github.com>
2025-01-13 02:09:37 -0700

    Time: 1014 ms (27.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+with cte as (
+    select employee_id, count(department_id) as cnt_dept
+    from employee
+    group by employee_id
+    having count(department_id) = 1
+)
+
+select employee_id, department_id
+from employee 
+where primary_flag = 'Y' or employee_id in (select employee_id from cte)
