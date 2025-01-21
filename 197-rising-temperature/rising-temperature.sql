946fb62fd153c45b49e8ded206a557cca3c5a2c1
Hari Chandana Kotnani <122574307+Harichandana0301@users.noreply.github.com>
2025-01-20 22:20:23 -0700

    Time: 896 ms (43.05%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,11 +1,6 @@
 /* Write your T-SQL query statement below */
-SELECT 
-    today.id
-FROM 
-    weather yesterday
-JOIN 
-    weather today
-ON 
-    DATEDIFF(day, yesterday.recordDate, today.recordDate) = 1
-WHERE 
-    today.temperature > yesterday.temperature;
+select w1.id as id
+from weather w1
+join weather w2
+on DATEDIFF(day,w2.recordDate, w1.recordDate) = 1
+where w1.temperature > w2.temperature
