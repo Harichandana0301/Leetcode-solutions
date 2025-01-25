ef0d8d036073a24cafe0a2328d0eb6d994eae1e6
Hari Chandana Kotnani <122574307+Harichandana0301@users.noreply.github.com>
2025-01-25 13:02:15 -0700

    Time: 935 ms (52.75%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,17 +1,8 @@
-WITH cte AS (
-    SELECT 
-        s.user_id,
-        CASE 
-            WHEN c.action = 'confirmed' THEN 1 
-            ELSE 0 
-        END AS confirmed
-    FROM signups s
-    LEFT JOIN confirmations c
-        ON s.user_id = c.user_id
-)
-SELECT 
-    user_id, 
-    ROUND(AVG(CAST(confirmed AS FLOAT)), 2) AS confirmation_rate
-FROM cte
-GROUP BY user_id
-ORDER BY user_id;
+/* Write your T-SQL query statement below */
+select s.user_id, round(avg(case when c.action='Confirmed' then 1.0 else 0.0 end),2) as confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
+
+
