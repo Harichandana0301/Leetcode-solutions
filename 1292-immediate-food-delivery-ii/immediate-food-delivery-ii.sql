d4424fb87aae9e1714776744588b219b8ba3ae5a
Hari Chandana Kotnani <122574307+Harichandana0301@users.noreply.github.com>
2025-01-27 15:36:59 -0700

    Time: 3029 ms (14.25%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,10 +1,10 @@
 /* Write your T-SQL query statement below */
-with cte as(
-    select *, row_number() over(partition by customer_id order by order_date) as rn
+with min_date as(
+    select *,row_number() over (partition by customer_id order by order_date) as date
     from delivery
 )
 
 select
 round(sum(case when order_date = customer_pref_delivery_date then 1.0 else 0 end) *100 / count(customer_id),2) as immediate_percentage
-from cte
-where rn = 1
+from min_date
+where date = 1
