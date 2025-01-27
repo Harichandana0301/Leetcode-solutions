4ac6179b7b00d38fabaf124b6b9b89910b1ab321
Hari Chandana Kotnani <122574307+Harichandana0301@users.noreply.github.com>
2025-01-27 13:50:43 -0700

    Time: 4315 ms (7.98%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
-
-select p.project_id, round(sum(experience_years) *1.0/count(project_id),2) as average_years
-From project p
+select 
+p.project_id, round(sum(e.experience_years)*1.0/ count(p.employee_id),2) as average_years
+from project p
 join employee e
-on p.employee_id = e.employee_id 
+on p.employee_id = e.employee_id
 group by p.project_id
