860318d59959c66a6175766960102e76eebafc29
Hari Chandana Kotnani <122574307+Harichandana0301@users.noreply.github.com>
2025-02-04 01:48:59 -0700

    Time: 1039 ms (42.83%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,10 +1,13 @@
 /* Write your T-SQL query statement below */
- with empmod as(
-     select DENSE_rank() over(partition by departmentId order by salary desc ) as rank,
-     id, name, salary, departmentId
-     from Employee
- ) 
+with cte as(
+    select e.id as id ,d.name as  Department, e.name as employee, 
+dense_rank() over(partition by e.departmentid order by e.salary desc) as rank
+from employee e join
+department d
+on e.departmentid = d.id
+)
 
- select d.name as Department , e.name as Employee, e.Salary
- from empmod e join Department d
- on e.departmentId = d.id and rank <= 3
+select department, employee, salary from
+cte c join employee e
+on c.id = e.id
+where rank <= 3
